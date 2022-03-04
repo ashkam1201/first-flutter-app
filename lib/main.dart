f2b76871dae3a4a01417cb3c975347f13baa0345
unknown <adrianhagjija@hotmail.com>
2022-03-04 17:49:10 +0100

    day 2 of coding the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+import './answer.dart';
+
 //void main() {
 //  runApp(MyApp());
 //}
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  var questionIndex = 0;
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
 
-  void answerQuestion() {
-    questionIndex = questionIndex + 1;
-    print(questionIndex);
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
   }
 
   @override
@@ -27,23 +40,12 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text(
-              questions[questionIndex],
+            Question(
+              questions[_questionIndex],
             ),
-            RaisedButton(
-              child: Text('Answer1'),
-              onPressed: () => print('answer 1 chosen'),
-            ),
-            RaisedButton(
-              child: Text('Answer2'),
-              onPressed: () {
-                print('answer 2 chosen');
-              },
-            ),
-            RaisedButton(
-              child: Text('Answer3'),
-              onPressed: answerQuestion,
-            ),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
           ],
         ),
       ),
